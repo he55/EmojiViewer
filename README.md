6533655731a8b03e3718cb73e289fcb1b357b073
he55 <40764272+he55@users.noreply.github.com>
2024-01-25 22:54:47 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# EmojiViewer
+<div align="center">
+  <br />
+
+  <img src="icon.png" width="256">
+  <h1>EmojiViewer</h1>
+  <p>
+    Emoji 表情查看器。拥有来自 fluentui-emoji 的 1545 个表情符号。
+  </p>
+  
+  <a href="https://github.com/he55/EmojiViewer/releases/latest">
+    <img src="https://img.shields.io/github/release/he55/EmojiViewer.svg?style=flat-square">
+  </a>
+  <br />
+  <br />
+
+  <img src="screenshot.png" width="915">
+  <br />
+  
+</div>
+
+## 下载
+https://github.com/he55/EmojiViewer/releases/latest
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EmojiViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmojiAsset _lastSelectedAsset;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<EmojiAsset> assets = LoadData(@"C:\Users\admin\Documents\GitHub\fluentui-emoji\assets");
+            List<EmojiCategory> categories = assets.GroupBy(x => x.emoji.group)
+                .Select(x => new EmojiCategory { title = x.Key, assets = x.ToList() })
+                .ToList();
+            listBox.ItemsSource = categories;
+        }
+
+        static List<EmojiAsset> LoadData(string path)
+        {
+            List<EmojiAsset> assets = new List<EmojiAsset>();
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                string imageDir = Path.Combine(dir, "3D");
+                if (!Directory.Exists(imageDir))
+                    imageDir = Path.Combine(dir, @"Default\3D");
+
+                var files = Directory.GetFiles(imageDir, "*.png");
+                if (files.Length == 0)
+                    continue;
+
+                string filePath = Path.Combine(dir, "metadata.json");
+                string json = File.ReadAllText(filePath);
+                EmojiObject emoji = TinyJson.JSONParser.FromJson<EmojiObject>(json);
+
+                EmojiAsset asset = new EmojiAsset();
+                asset.emoji = emoji;
+                asset.id = dir;
+                asset.previewImage = files[0];
+                asset.name = Path.GetFileName(dir);
+                assets.Add(asset);
+            }
+
+            return assets;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gridView.ItemsSource = ((EmojiCategory)listBox.SelectedItem).assets;
+            if (gridView.Items.Count > 0)
+                gridView.ScrollIntoView(gridView.Items[0]);
+        }
+
+        private void gridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EmojiAsset selectedItem = (EmojiAsset)gridView.SelectedItem;
+            if (selectedItem != null)
+            {
+                if (_lastSelectedAsset != null)
+                    _lastSelectedAsset.isSelected = false;
+
+                selectedItem.isSelected = true;
+                header.DataContext = selectedItem;
+                _lastSelectedAsset = selectedItem;
+
+                listBox2.SelectedIndex = selectedItem.items.Count > 1 ? 1 : 0;
+                listBox2.Visibility = selectedItem.items.Count > 1 ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AssetItem selectedItem = (AssetItem)listBox2.SelectedItem;
+            if (selectedItem != null)
+            {
+                Process.Start("explorer.exe", $"/select, \"{selectedItem.subitems[0].source}\"");
+            }
+        }
+    }
+}
